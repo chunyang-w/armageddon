a0d2475d9bfecc543f8974df8b18c358b862b79e
acse-2030c09f <arnav.avad@outlook.com>
2022-11-24 21:34:09 +0000

    mapper docstring completed

--- armageddon/mapping.py
+++ armageddon/mapping.py
@@ -40,6 +40,39 @@ def plot_circle(lat, lon, radius, map=None, **kwargs):
 
 
 def damage_map(blat, blon, damrad, lat, lon):
+    """
+    Plot circles on a map as needed (creating a new folium map instance if necessary).
+
+    Parameters
+    ----------
+
+    blat: float
+        latitude of circle to plot (degrees)
+    blon: float
+        longitude of circle to plot (degrees)
+    damrad: arraylike
+    lat: float
+        the entry point of the meteorite as a latitude (degrees)
+    lon: float
+        the entry point of the meteorite as a longitude (degrees)
+
+    Returns
+    -------
+
+    Folium map object
+
+    Examples
+    --------
+
+    >>> import folium
+    >>> outcome = {'burst_altitude': 8e3, 'burst_energy': 7e3,\
+                   'burst_distance': 90e3, 'burst_peak_dedz': 1e3,\
+                   'outcome': 'Airburst'}
+    >>> lat = 52.79
+    >>> lon = -2.95
+    >>> blat, blon, damrad = armageddon.damage_zones(outcome, lat, lon, 135, pressures=[1e3, 3.5e3, 27e3, 43e3])
+    >>> armageddon.damage_map(blat, blon, damrad, lat, lon)
+    """
     damrad = damrad[::-1]
     for rad_index in range(len(damrad)):
         if rad_index == 0:
