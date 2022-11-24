e8b92498bf0c8d4e3e82cb5d7125053a06ab6239
chunyang.w <cw1722@ic.ac.uk>
2022-11-24 15:45:54 +0000

    temp save

--- gui/interface.py
+++ gui/interface.py
@@ -2,13 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 window = tk.Tk()
 window.title("Welcome to TutorialsPoint")
-window.geometry('400x400')
+window.geometry('800x800')
 window.configure()
-a = tk.Label(window, text="First Name").grid(row=0, column=0)
+
+a = tk.Label(window, text="Radius").grid(row=0, column=0)
+a1 = tk.Entry(window).grid(row=0, column=1)
+
 b = tk.Label(window, text="Last Name").grid(row=1, column=0)
 c = tk.Label(window, text="Email Id").grid(row=2, column=0)
-d = tk.Label(window, text="Contact Number").grid(row=3,column=0)
-a1 = tk.Entry(window).grid(row=0, column=1)
+d = tk.Label(window, text="Contact Number").grid(row=3, column=0)
+
+
 b1 = tk.Entry(window).grid(row=1, column=1)
 c1 = tk.Entry(window).grid(row=2, column=1)
 d1 = tk.Entry(window).grid(row=3, column=1)
