8c97c6c92d0b648cee694a83db443121a6d236ea
githublinux <github@linux>
2022-11-24 14:06:57 +0000

    update html and pdf through wrokflow

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,10 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-<<<<<<< HEAD
-    VERSION: '',
-=======
     VERSION: '0.0.1',
->>>>>>> main
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
